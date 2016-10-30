6d04039c94ebc0229829663eaa911a25ff2e2c28
Manuel <m.moracuesta@gmail.com>
2016-10-30 20:34:07 +0100

    Tutorial

---
+++ Assets/Scripts/DisableOnTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableOnTime : MonoBehaviour {
+
+    public float timeToDisable;
+    float m_acumTime = 0;
+
+    public void OnEnable()
+    {
+        m_acumTime = 0;
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        m_acumTime += Time.deltaTime;
+        if (m_acumTime > timeToDisable)
+            gameObject.SetActive(false);
+	}
+}
