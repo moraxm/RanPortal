befa7ee21b25d62a68156ff2c92336d0adaf9c8c
Manuel <m.moracuesta@gmail.com>
2016-11-06 17:29:56 +0100

    Added shared button

---
+++ Assets/Scripts/UI/ShareButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShareButton : MonoBehaviour
+{
+
+    public void Share()
+    {
+        string message = "I have achieved "+ GameManager.instance.points.ToString() + " points in #RandomPortal";
+        string subject = "Random Portal";
+        //execute the below lines if being run on a Android device
+#if UNITY_ANDROID
+        //Reference of AndroidJavaClass class for intent
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        //Reference of AndroidJavaObject class for intent
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        //call setAction method of the Intent object created
+        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        //set the type of sharing that is happening
+        intentObject.Call<AndroidJavaObject>("setType", "text/plain");
+        //add data to be passed to the other activity i.e., the data to be sent
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), subject);
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), message);
+        //get the current activity
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        //start the activity by sending the intent data
+        AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Via");
+        currentActivity.Call("startActivity", jChooser);
+#endif
+
+    }
+}
