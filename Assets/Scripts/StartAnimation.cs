5869f0a73b862b51f70aed4190f92998d9539669
Manuel <m.moracuesta@gmail.com>
2016-11-06 19:46:44 +0100

    Start animation ball for move up the ball at the beginning

---
+++ Assets/Scripts/StartAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartAnimation : MonoBehaviour {
+    public Transform startPosition;
+    public Transform finalPosition;
+    public float time = 1;
+    float m_acumTime = 0;
+
+    public void OnEnable()
+    {
+        m_acumTime = 0;
+        RestartPosition();
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        m_acumTime += Time.deltaTime;
+        float f = m_acumTime / time;
+        transform.position = Vector3.Lerp(startPosition.position, finalPosition.position, f);
+        if (f > 1) enabled = false;
+	}
+
+    public void RestartPosition()
+    {
+        transform.position = startPosition.position;
+    }
+}
