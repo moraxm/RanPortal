2ffe283dd456dbcfbd98679feec549ce559eb400
Manuel <m.moracuesta@gmail.com>
2016-11-15 19:32:58 +0100

    Image buttons for login/out. Changed ball start position

--- Assets/Scripts/UI/LogInButton.cs
+++ Assets/Scripts/UI/LogInButton.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 public class LogInButton : MonoBehaviour 
 {
     public Color logout;
+    public Sprite logoutSprite;
     public Color login;
+    public Sprite loginSprite;
     public Color authenticating;
 
     Text m_textComponent;
@@ -26,13 +28,22 @@ public class LogInButton : MonoBehaviour
 
     public void Update()
     {
-        m_textComponent.text = PlayGamesServiceManager.instance.isAuthenticated ? "Log out" : "Log In";
+        m_textComponent.text = PlayGamesServiceManager.instance.isAuthenticated ? "LOG OUT" : "LOG IN";
         if (PlayGamesServiceManager.instance.authenticating)
+        {
             m_ImageComponent.color = authenticating;
+            m_ImageComponent.sprite = logoutSprite;
+        }
         else if (PlayGamesServiceManager.instance.isAuthenticated)
+        {
             m_ImageComponent.color = login;
+            m_ImageComponent.sprite = loginSprite;
+        }
         else
+        {
             m_ImageComponent.color = logout;
+            m_ImageComponent.sprite = logoutSprite;
+        }
     }
 
     public void Login()
